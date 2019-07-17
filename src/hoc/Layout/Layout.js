2ebbfd6f2f8e647b4203d55b1e327625f8383a16
Kate Shaitarova <eshaytarova@gmail.com>
2019-07-17 20:58:10 +1200

    Set up react app and initialize layout

---
+++ src/hoc/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Layout.module.css';
+
+const Layout = () => {
+    return (
+        <div className={styles.Layout}>
+            Content
+        </div>
+    );
+}
+
+export default Layout;
