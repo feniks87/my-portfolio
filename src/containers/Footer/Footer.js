cff3b448e44f05b1e7eec593f0c7549ae6ecaa68
Kate Shaitarova <eshaytarova@gmail.com>
2019-08-13 15:32:40 +1200

    Fix small issues
    - Issue with z-index of Project image
    - Issue with instagram link

--- src/containers/Footer/Footer.js
+++ src/containers/Footer/Footer.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import styles from './Footer.module.css';
 
-import { ReactComponent as LinkeinIcon } from '../../assets/svg/linkedin.svg';
+import { ReactComponent as LinkedinIcon } from '../../assets/svg/linkedin.svg';
 import { ReactComponent as InstaIcon } from '../../assets/svg/instagram.svg';
 import { ReactComponent as FacebookIcon } from '../../assets/svg/facebook.svg';
 import { ReactComponent as GithubIcon } from '../../assets/svg/github.svg';
@@ -10,9 +10,9 @@ const Footer = () => (
     <footer className={styles.Footer}>
         <div className={styles.FoterContent}>
             <div className={styles.FooterIcons}>
-                <a target="_blank" href="https://www.linkedin.com/in/kate-shaitarova-574655138/"><LinkeinIcon className={styles.Icon}/></a>
+                <a target="_blank" href="https://www.linkedin.com/in/kate-shaitarova-574655138/"><LinkedinIcon className={styles.Icon}/></a>
                 <a target="_blank" href="https://www.facebook.com/shaytarova?ref=bookmarks"><FacebookIcon className={styles.Icon}/></a>
-                <a target="_blank" href="https://www.instagram.com/eshaytarova/?hl=en"><InstaIcon className={styles.Icon}/></a>
+                <a target="_blank" href="https://www.instagram.com/kate_shaitarova/"><InstaIcon className={styles.Icon}/></a>
                 <a target="_blank" href="https://github.com/feniks87"><GithubIcon className={styles.Icon}/></a>
             </div>
             <div className={styles.Copyright}>
